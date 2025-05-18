fc2bd772102bae56d8e152efdb7934ede1b04abb
mophima <contact@moritzphilippmaier.de>
2025-05-18 13:02:29 +0200

    Initial commit:extracted from flowrs repo

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+mod connectable;
+
+use proc_macro::TokenStream;
+
+use connectable::impl_connectable_trait;
+
+#[proc_macro_derive(RuntimeConnectable, attributes(input, output))]
+pub fn connectable_derive_macro(item: TokenStream) -> TokenStream {
+    let ast = syn::parse(item).unwrap();
+
+    impl_connectable_trait(ast)
+}
